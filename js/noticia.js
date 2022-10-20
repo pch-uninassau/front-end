80ad45d3fea1090ac2a781ba56b2ec9f5a572a68
Gustavo <gstvrrds@gmail.com>
2022-10-20 12:40:42 -0300

    trocando background, cores das fontes da noticia, colocando para buscar as imagens das noticias com js

--- js/noticia.js
+++ js/noticia.js
@@ -3,18 +3,22 @@ var noticia = {
     {
       titulo: "TITULO1",
       descricao: "lorem ipsum 1",
+      img :"/assets/bg-01.jpg"
     },
     {
       titulo: "TITULO2",
       descricao: "lorem ipsum 2",
+      img :"/assets/fotoNoticia.svg"
     },
     {
       titulo: "TITULO3",
       descricao: "lorem ipsum 3 ",
+      img :"/assets/bg-01.jpg"
     },
     {
       titulo: "TITULO4",
       descricao: "lorem ipsum 4",
+      img :"/assets/bg-01.jpg"
     },
   ],
 };
@@ -27,3 +31,5 @@ var titulo = document.querySelector(".titulo__noticia").textContent = noticia.ti
 
 var descricao = document.querySelector(".paragrafo__noticia").textContent = noticia.descricao;
  
+var noticiasImagem = document.querySelector(".NoticiaImagem").src = noticia.img;
+console.log(noticiasImagem)

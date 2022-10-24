baa216b78f419bb26ed70c593dd575dfb6496d0b
Gustavo <gstvrrds@gmail.com>
2022-10-24 15:16:17 -0300

    adicionando grid na área do perfil do header

--- js/menu--lateral.js
+++ js/menu--lateral.js
@@ -1,6 +1,7 @@
 var excluirMenu = document.querySelector(".excluir");
 var menuLateral = document.querySelector(".menu-lateral-mobile");
 var iconeMenu = document.querySelector(".menu");
+
 excluirMenu.addEventListener("click", () => {
     menuLateral.classList.add("menu--escondido");
     menuLateral.classList.remove("menu--ativo");
@@ -9,5 +10,4 @@ excluirMenu.addEventListener("click", () => {
 iconeMenu.addEventListener("click", () => {
     menuLateral.classList.add("menu--ativo");
     menuLateral.classList.remove("menu--escondido");
-
 })
